c85b7924bf8f5797140ee568d22a0e6110fd1fd9
Dmitry <yanyushin.dim@gmail.com>
2022-02-20 14:29:13 +0300

    cicles

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?', '');
 
@@ -10,12 +10,30 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последнгих просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последнгих просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i< 2; i++){
+    const a = prompt('Один из последнгих просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log("try"); 
+    } else {
+        console.log("Error"); 
+        i--;
+    }
+    personalMovieDB.movies[a] = b;      
+}
+
+
+if (personalMovieDB.count < 10) {
+    prompt('Просмотренно довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    prompt('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    prompt('Вы киноман');
+} else {
+    prompt('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
